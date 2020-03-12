9f1adf6c003a7ad93505e162132cf488b236924e
JuviaBerry <47364179+JuviaBerry@users.noreply.github.com>
2020-03-12 12:14:34 +0100

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("salut salut");
+        
+         // création de la fenêtre
+        Window FenetrePrincipal= new Window();
+        
     }
 }
