73e03dc66fa20189a8ea8b8eb8ed84b2c35b0d27
crackerdead <58956768+crackerdead@users.noreply.github.com>
2020-03-19 00:37:09 +0100

    Add files via upload

---
+++ tout cassé/README.md
@@ -0,0 +1,1 @@
+# Fourier-le-vrai-guerrier
---
+++ tout cassé/Window.java
@@ -0,0 +1,116 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.LinkedList;
+import javax.swing.*;
+
+
+public class Window extends JFrame implements ActionListener {
+
+    DispPan panelshow;
+    JButton buttonstart;
+    JButton buttonclear;
+    JSlider curseurpreci;
+    JTextArea scroll;
+    JScrollPane scrollequation;
+    JLabel precitext;
+    PanneauEntree paneldraw;
+
+
+    public Window(){
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLayout(null);
+        /*Fenetre principale*/
+        int screenW= screenSize.width;
+        int screenH= screenSize.height;
+        double c = (double)screenH/1080 ; //coef de grandissement
+
+        setLayout(null);
+        setTitle("Projet guerrier- Fenetre principale");
+        setLocation((int)((screenW-(1800*c))/2),(int)((screenH-(900*c))/2));
+        setSize((int)(1800*c),(int)(900*c));
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        /*Panel dessin*/
+        paneldraw = new PanneauEntree();
+        paneldraw.setLayout(null);
+        paneldraw.setBounds((int)(100*c),(int)(50*c),(int)(700*c),(int)(500*c));
+
+        /*Panel affichage*/
+        panelshow = new DispPan(new LinkedList<Complexe>());
+        panelshow.setLayout(null);
+        panelshow.setBounds((int)(c*(100+200+700)),(int)(50*c),(int)(700*c),(int)(500*c));
+
+        /*Les boutons*/
+        buttonstart = new JButton();
+        buttonstart.setText("start");
+        buttonstart.setBounds((int)(200*c),(int)(c*(50+500+50)),(int)(200*c),(int)(75*c));
+        buttonclear = new JButton();
+        buttonclear.setText("clear");
+        buttonclear.setBounds((int)(200*c+100*c+200*c),(int)(c*(50+500+50)),(int)(200*c),(int)(75*c));
+
+        /*Le curseur pour regler la precision*/ //à modifier en curseur après
+        curseurpreci = new JSlider();
+        precitext = new JLabel();
+        precitext.setText("PRECISION: ");
+        precitext.setBounds((int)(c*(100+700+200+100+210)),(int)(c*(500+50+20)),(int)(c*250),(int)(c*25));
+        curseurpreci.setBounds((int)(c*(100+700+200+100)),(int)(c*(50+500+50)),(int)(c*500),(int)(c*75));
+
+        /*Affichage de l'équation*/
+        scroll= new JTextArea();
+        scrollequation = new JScrollPane(scroll);
+        scrollequation.setBounds((int)(c*(100)),(int)(c*(50+500+50+75+50)),(int)(c*1600),(int)(c*75));
+
+        /*Création du Panneau d'entrée*/
+        PanneauEntree pEntree = new PanneauEntree(); /*A PLACER*/
+
+        /*Ajout à la fenetre principale*/
+        this.add(paneldraw);
+        this.add(panelshow);
+        this.add(buttonstart);
+        this.add(buttonclear);
+        this.add(curseurpreci);
+        this.add(scrollequation);
+
+        /*Ajout au ActionListener*/
+        buttonstart.addActionListener(this);
+        buttonclear.addActionListener(this);
+
+    }
+
+    public void actionPerformed (ActionEvent e){
+
+        if ((e.getSource() == buttonstart)) {
+			panelshow.ligne = tFourier(paneldraw.listepoints, 100);
+			panelshow.setOpaque(true);
+			panelshow.setVisible(true);
+			panelshow.setOpaque(true);
+			panelshow.repaint();
+        }
+
+        if ((e.getSource() == buttonclear)) {
+        }
+
+    }
+
+    public static LinkedList<Complexe> tFourier(LinkedList<Complexe> l, int nbVect){	//méthode qui effectue la transformée de fourier
+		double nbPoints = l.size();
+		Complexe integrale = new Complexe(0,0);
+		LinkedList<Complexe> renvoi = new LinkedList<Complexe>();
+		for(int i = 0 ; i < nbVect ; i++){
+			integrale = new Complexe(0,0);
+			double t = 0;
+		for(Complexe c : l){
+			Complexe sc = new Complexe(1, c.getRho(), c.getTheta() - (i*Math.PI*2*t/nbPoints)); 
+			integrale.sommeV1(sc);
+			t++;
+		}
+		integrale.setRho(integrale.getRho()/(nbPoints));
+		renvoi.add(integrale);
+	}
+		return(renvoi);
+	}
+
+}
