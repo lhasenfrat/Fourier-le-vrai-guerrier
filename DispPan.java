e076c56dc9a565099582b93b6e77efebc849b101
lhasenfrat <59894122+lhasenfrat@users.noreply.github.com>
2020-03-26 08:45:53 +0100

    Update DispPan.java

--- DispPan.java
+++ DispPan.java
@@ -10,7 +10,10 @@ public class DispPan extends JPanel implements ActionListener{
 	public LinkedList<Complexe> dessin;
 	private double angleRot;
 
+<<<<<<< HEAD
 	
+=======
+>>>>>>> cd35152cd72277edfe45485f3b1b6ddd3d1e3b4a
 	public DispPan(LinkedList<Complexe> d){
 		this.ligne = d;
 		this.dessin = new LinkedList<Complexe>();
@@ -18,6 +21,7 @@ public class DispPan extends JPanel implements ActionListener{
 		chrono = new Timer(50, this);
 		chrono.start();
 	}
+<<<<<<< HEAD
 	public DispPan(){
 		this.ligne = new LinkedList<Complexe>();
 		this.dessin = new LinkedList<Complexe>();
@@ -28,6 +32,9 @@ public class DispPan extends JPanel implements ActionListener{
 
 	public void actionPerformed(ActionEvent e){
 
+=======
+	public void actionPerformed(ActionEvent k){
+>>>>>>> cd35152cd72277edfe45485f3b1b6ddd3d1e3b4a
 		int i = 0;
 		Complexe lineEnd = new Complexe(0,0);
 		for(Complexe c : ligne){
@@ -39,23 +46,40 @@ public class DispPan extends JPanel implements ActionListener{
 		this.repaint();
 	}
 	
+<<<<<<< HEAD
 	public void paint(Graphics g){
 
 		g.setColor(Color.white);
 		g.fillRect(0,0,getWidth(),getHeight());
         g.setColor(Color.black);
+=======
+	public void paint(Graphics h){
+		h.setColor(Color.white);
+		h.fillRect(0,0,getWidth(),getHeight());
+		h.setColor(Color.black);
+>>>>>>> cd35152cd72277edfe45485f3b1b6ddd3d1e3b4a
 		Complexe point1 = new Complexe(this.getWidth()/2, this.getHeight()/2);	//on place le premier point au milieu du panneau pour tracer le premier trait
 		for(Complexe c : ligne){
-			g.drawLine((int)(point1.getRe()), (int)(point1.getIm()), (int)(c.getRe() + point1.getRe()), (int)(c.getIm() + point1.getIm()));
+			h.drawLine((int)(point1.getRe()), (int)(point1.getIm()), (int)(c.getRe() + point1.getRe()), (int)(c.getIm() + point1.getIm()));
 			point1 = new Complexe(c.getRe() + point1.getRe(), c.getIm() + point1.getIm());
 		}
 		point1 = new Complexe(this.getWidth()/2 + (int)(dessin.getFirst().getRe()), this.getHeight()/2 + (int)(dessin.getFirst().getIm()));
+		Complexe point0 = new Complexe(this.getWidth()/2 + (int)(dessin.getFirst().getRe()), this.getHeight()/2 + (int)(dessin.getFirst().getIm()));
 		for(Complexe c : dessin){
+<<<<<<< HEAD
 
 			g.drawLine((int)(point1.getRe()), (int)(point1.getIm()), (int)(c.getRe() + this.getWidth()/2), (int)(c.getIm()) + this.getHeight()/2);
+=======
+			h.setColor(Color.red);
+			if (point1.equals(point0)) {
+				h.setColor(Color.white); //de même, pour enlever le premier trait rouge qui connecte avec le milieu
+			}
+			h.drawLine((int)(point1.getRe()), (int)(point1.getIm()), (int)(c.getRe() + this.getWidth()/2), (int)(c.getIm()) + this.getHeight()/2);
+>>>>>>> cd35152cd72277edfe45485f3b1b6ddd3d1e3b4a
 			point1 = new Complexe(c.getRe() + this.getWidth()/2, c.getIm() + this.getHeight()/2);
 		}
 		
 	}
+
 	
 }
