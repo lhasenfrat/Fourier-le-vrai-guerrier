2adfb1d0300371959b601207e6edfd5b0aa39865
lhasenfrat <59894122+lhasenfrat@users.noreply.github.com>
2020-03-27 11:27:58 +0100

    cbo

---
+++ TemplateButton.java
@@ -0,0 +1,58 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class TemplateButton extends JButton {
+    boolean carre=false;
+    boolean hexa=false;
+    boolean losange=false;
+    boolean formelouche=false;
+    public TemplateButton(String forme)
+    {
+
+        setFont(new Font("Arial",Font.BOLD,50/2));
+
+        if (forme == "carre")
+        {
+            carre=true;
+
+        } else if (forme == "hexa")
+        {
+            hexa=true;
+
+        } else if (forme == "losange")
+        {
+            losange=true;
+
+        } else if (forme == "formelouche")
+        {
+            formelouche=true;
+
+        }
+
+    }
+    public void paintComponent(Graphics g)
+    {
+        g.setColor(Color.WHITE);
+        g.fillRect(0,0,getWidth(),getHeight());
+        g.setColor(Color.black);
+
+        if (carre)
+        {
+            g.drawRect(getWidth()/4,getHeight()/4,getWidth()/2,getHeight()/2);
+        } else if(hexa)
+        {
+            g.drawOval(getWidth()/4,getHeight()/4,getWidth()/2,getHeight()/2);
+        } else if(losange)
+        {
+            g.drawLine(getWidth()/2,getHeight()/4,3*getWidth()/4,getHeight()/2);
+            g.drawLine(3*getWidth()/4,getHeight()/2,getWidth()/2,3*getHeight()/4);
+            g.drawLine(getWidth()/2,getHeight()*3/4,getWidth()/4,getHeight()/2);
+            g.drawLine(getWidth()/4,getHeight()/2,getWidth()/2,getHeight()/4);
+
+        } else if(formelouche)
+        {
+            g.drawString("?",4*getWidth()/10,6*getWidth()/10);
+
+        }
+    }
+}
--- Window.java
+++ Window.java
@@ -1,8 +1,10 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Hashtable;
 import java.util.LinkedList;
 import javax.swing.*;
-import java.util.Hashtable;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 
 public class Window extends JFrame implements ActionListener {
@@ -14,7 +16,16 @@ public class Window extends JFrame implements ActionListener {
     JTextArea scroll;
     JScrollPane scrollequation;
     JLabel precitext;
+    JLabel enter;
+    JLabel exit;
+    JLabel logo_INSA;
+    JLabel title_label;
     PanneauEntree paneldraw;
+    TemplateButton boutoncarre;
+    TemplateButton boutonhexa;
+    TemplateButton boutonlosange;
+    TemplateButton boutonrandom;
+
 
     double c;
 
@@ -24,7 +35,7 @@ public class Window extends JFrame implements ActionListener {
         /*Fenetre principale*/
         int screenW= screenSize.width;
         int screenH= screenSize.height;
-        c = (double)screenH/1080 ; //coef de grandissement
+        double c = (double)screenH/1080 ; //coef de grandissement
 
         setLayout(null);
         setTitle("Projet guerrier- Fenetre principale");
@@ -32,55 +43,97 @@ public class Window extends JFrame implements ActionListener {
         setSize((int)(1800*c),(int)(900*c));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
+        setBackground(Color.MAGENTA);
+        getContentPane().setBackground(Color.gray);
 
         /*Panel dessin*/
         paneldraw = new PanneauEntree();
-
-
         paneldraw.setBounds((int)(100*c),(int)(50*c),(int)(700*c),(int)(500*c));
 
         /*Panel affichage*/
         panelshow = new DispPan();
         panelshow.setBackground(Color.white);
-
         panelshow.setBounds((int)(c*(100+200+700)),(int)(50*c),(int)(700*c),(int)(500*c));
 
         /*Les boutons*/
         buttonstart = new JButton();
-        buttonstart.setText("start");
+        buttonstart.setText("Start");
         buttonstart.setBounds((int)(200*c),(int)(c*(50+500+50)),(int)(200*c),(int)(75*c));
+        buttonstart.setBackground(Color.green);
+        buttonstart.setFont(new Font("Arial",Font.BOLD,20));
+        buttonstart.setBorder(BorderFactory.createLineBorder(Color.black, 5)); // Line Border + Thickness of the Border
+
         buttonclear = new JButton();
-        buttonclear.setText("clear");
+        buttonclear.setText("Clear");
         buttonclear.setBounds((int)(200*c+100*c+200*c),(int)(c*(50+500+50)),(int)(200*c),(int)(75*c));
+        buttonclear.setBackground(Color.red);
+        buttonclear.setFont(new Font("Arial",Font.BOLD,20));
+        buttonclear.setBorder(BorderFactory.createLineBorder(Color.black, 5));
 
-        /*Le curseur pour regler la precision*/ //à modifier en curseur après
-        curseurpreci = new JSlider(0,100,50);
+        boutoncarre = new TemplateButton("carre");
+        boutoncarre.setBounds((int)(850*c),(int)(60*c),(int)(100*c),(int)(100*c));
+
+        boutonhexa=new TemplateButton("hexa");
+        boutonhexa.setBounds((int)(850*c),(int)(185*c),(int)(100*c),(int)(100*c));
+
+        boutonlosange=new TemplateButton("losange");
+        boutonlosange.setBounds((int)(850*c),(int)(310*c),(int)(100*c),(int)(100*c));
+
+
+        boutonrandom=new TemplateButton("formelouche");
+        boutonrandom.setBounds((int)(850*c),(int)(435*c),(int)(100*c),(int)(100*c));
+
+
+        /*Le curseur pour regler la precision*/
+        curseurpreci = new JSlider(0,100);
         curseurpreci.setMajorTickSpacing(25);
-        curseurpreci.setPaintTicks(true);
-
-
-        /*Autre info vers le curseur*/
-        precitext = new JLabel();
-        precitext.setText("PRECISION: ");
-        precitext.setBounds((int)(c*(100+700+200+100+210)),(int)(c*(500+50+20)),(int)(c*250),(int)(c*25));
+        curseurpreci.setMinorTickSpacing(5);
         curseurpreci.setBounds((int)(c*(100+700+200+100)),(int)(c*(50+500+50)),(int)(c*500),(int)(c*75));
-        // Add positions label in the slider
+        curseurpreci.setBorder(BorderFactory.createLineBorder(Color.black, 5));
+
+        /*Ajout position label sur glisseur*/
         curseurpreci.setPaintLabels(true);
         Hashtable position = new Hashtable();
-        position.put(0, new JLabel("0"));
-        position.put(25, new JLabel("25"));
-        position.put(50, new JLabel("50"));
-        position.put(75, new JLabel("75"));
-        position.put(100, new JLabel("100"));
+        position.put(0,new JLabel("0"));
+        position.put(25,new JLabel("25"));
+        position.put(50,new JLabel("50"));
+        position.put(75,new JLabel("75"));
+        position.put(100,new JLabel("100"));
         curseurpreci.setLabelTable(position);
+        curseurpreci.setPaintTicks(true);
 
-
-        /*Affichage de l'équation*/
+        /*Affichage de l'Ã©quation*/
         scroll= new JTextArea();
+        scroll.setText("En attente...");
+        scroll.setFont(new Font("Arial",Font.ITALIC,30)); //Texte affichÃ© Ã  l'initialisation
+
         scrollequation = new JScrollPane(scroll);
         scrollequation.setBounds((int)(c*(100)),(int)(c*(50+500+50+75+50)),(int)(c*1600),(int)(c*75));
+        scrollequation.setBorder(BorderFactory.createLineBorder(Color.black, 5));
 
-        /*Ajout à la fenetre principale*/
+        /*Les Labels*/
+        Font police = new Font("Arial",Font.BOLD,20); //Police Label (Arial, taille 20)
+        enter = new JLabel("Dessiner ici");
+        enter.setFont(police);
+        enter.setBounds((int)(350*c),(int)(1*c),(int)(500*c),(int)(50*c));
+
+        exit = new JLabel("Affichage");
+        exit.setFont(police);
+        exit.setBounds((int)(1300*c),(int)(1*c),(int)(500*c),(int)(50*c));
+
+        precitext = new JLabel();
+        precitext.setText("Precision : 50");
+        precitext.setFont(police);
+        precitext.setBounds((int)(c*(1250)),(int)(c*(500+50+20)),(int)(c*250),(int)(c*25));
+
+        logo_INSA = new JLabel(new ImageIcon("./src/Images/logo_INSA.png"));
+        logo_INSA.setBounds(0,screenH-300,100,100);
+
+        title_label = new JLabel("Projet guerrier : le dessin par Fourier");
+        title_label.setBounds(screenW-550,screenH-300,700,100);
+        title_label.setFont(new Font("Osaka",Font.ITALIC,25));
+
+        /*Ajout Ã  la fenetre principale*/
 
         this.add(paneldraw);
         this.add(panelshow);
@@ -88,10 +141,31 @@ public class Window extends JFrame implements ActionListener {
         this.add(buttonclear);
         this.add(curseurpreci);
         this.add(scrollequation);
+        this.add(enter);
+        this.add(exit);
+        this.add(precitext);
+        this.add(logo_INSA);
+        this.add(title_label);
+        this.add(boutonlosange);
+        this.add(boutonrandom);
+        this.add(boutonhexa);
+        this.add(boutoncarre);
 
-        /*Ajout au ActionListener*/
+
+        /*Ajout boutons au ActionListener*/
         buttonstart.addActionListener(this);
         buttonclear.addActionListener(this);
+        boutoncarre.addActionListener(this);
+        boutonlosange.addActionListener(this);
+        boutonhexa.addActionListener(this);
+        boutonrandom.addActionListener(this);
+        /*Ajout curseur au ChangeListener*/
+        curseurpreci.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                precitext.setText("Precision : " + String.valueOf(curseurpreci.getValue()));
+            }
+        });
 
 
         setVisible(true);
@@ -101,11 +175,8 @@ public class Window extends JFrame implements ActionListener {
     public void actionPerformed (ActionEvent e){
 
         if ((e.getSource() == buttonstart)) {
-            int nbrVect = paneldraw.listepoints.size(); //nbr de points du dessin dessiné
-            double precinum = (double)(curseurpreci.getValue())/(double)(100); //pourcentage de points par rapport à ceux du dessin dessiné
-            panelshow.ligne= tFourier(paneldraw.listepoints,(int)(precinum*nbrVect));
-            //System.out.println("nbr points : "+(int)(precinum*nbrVect));
-
+            panelshow.ligne= tFourier(paneldraw.listepoints, curseurpreci.getValue());
+            scroll.setText("");
 
         }
 
@@ -115,9 +186,58 @@ public class Window extends JFrame implements ActionListener {
 
         }
 
+        if (e.getSource()==boutoncarre) {
+            panelshow.ligne = new LinkedList<Complexe>();
+            panelshow.dessin = new LinkedList<Complexe>();
+            LinkedList<Complexe> listeDepart=new LinkedList<Complexe>();
+
+            for (int i = 0 ; i< 100; i++)										// test avec un carré
+                listeDepart.add(new Complexe(100,i));
+
+            for (int i = 0 ; i< 200; i++)
+                listeDepart.add(new Complexe(100 - i,100));
+            for (int i = 0 ; i< 200; i++)
+                listeDepart.add(new Complexe(-100,100-i));
+            for (int i = 0 ; i< 200; i++)
+                listeDepart.add(new Complexe(i - 100,-100));
+            for (int i = 0 ; i< 100; i++)
+                listeDepart.add(new Complexe(100,i - 100));
+            paneldraw.listepoints=listeDepart;
+
+            panelshow.ligne= tFourier(paneldraw.listepoints, curseurpreci.getValue());
+
+
+        } else  if (e.getSource()==boutonhexa) {
+            panelshow.ligne = new LinkedList<Complexe>();
+            panelshow.dessin = new LinkedList<Complexe>();
+
+        } else  if (e.getSource()==boutonlosange) {
+            panelshow.ligne = new LinkedList<Complexe>();
+            panelshow.dessin = new LinkedList<Complexe>();
+            LinkedList<Complexe> listeDepart=new LinkedList<Complexe>();
+
+            for (int i = 0 ; i< 200; i++)										// test avec un carré
+                listeDepart.add(new Complexe(200-i,i));
+
+            for (int i = 0 ; i< 200; i++)
+                listeDepart.add(new Complexe( - i,200-i));
+            for (int i = 0 ; i< 200; i++)
+                listeDepart.add(new Complexe(-200+i,-i));
+            for (int i = 0 ; i< 200; i++)
+                listeDepart.add(new Complexe(i,-200+i));
+
+            paneldraw.listepoints=listeDepart;
+
+            panelshow.ligne= tFourier(paneldraw.listepoints, curseurpreci.getValue());
+
+        } else  if (e.getSource()==boutonrandom) {
+            panelshow.ligne = new LinkedList<Complexe>();
+            panelshow.dessin = new LinkedList<Complexe>();
+
+        }
     }
 
-    public static LinkedList<Complexe> tFourier(LinkedList<Complexe> l, int nbVect){	//méthode qui effectue la transformée de fourier
+    public static LinkedList<Complexe> tFourier(LinkedList<Complexe> l, int nbVect){	//mÃ©thode qui effectue la transformÃ©e de fourier
         double nbPoints = l.size();
         Complexe integrale = new Complexe(0,0);
         LinkedList<Complexe> renvoi = new LinkedList<Complexe>();
