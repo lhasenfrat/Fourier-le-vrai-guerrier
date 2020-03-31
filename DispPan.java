0a578b2bd5667ce52293dae619c2f1a42c88e415
JuviaBerry <47364179+JuviaBerry@users.noreply.github.com>
2020-04-01 01:01:12 +0200

    Update DispPan.java

--- DispPan.java
+++ DispPan.java
@@ -5,63 +5,63 @@ import java.awt.event.*;
 import java.awt.Color;
 
 public class DispPan extends JPanel implements ActionListener{
-    public LinkedList<Complexe> ligne;
+    public LinkedList<Complex> ligne;
     public Timer chrono;
-    public LinkedList<Complexe> dessin;
+    public LinkedList<Complex> dessin;
     private double angleRot;
 
-	public DispPan(){
-		this.ligne = new LinkedList<Complexe>();
-		this.dessin = new LinkedList<Complexe>();
-		angleRot = 2*Math.PI/10000;
-		chrono = new Timer(5, this);
-		chrono.start(); 
-		try { 
-			Thread.sleep(100);
-		}
-		catch (InterruptedException exception) {
-			exception.printStackTrace();
-		}
-		chrono.stop();
+    public DispPan(){
+        this.ligne = new LinkedList<Complex>();
+        this.dessin = new LinkedList<Complex>();
+        angleRot = 2*Math.PI/10000;
+        chrono = new Timer(5, this);
+        chrono.start();
+        try {
+            Thread.sleep(100);
+        }
+        catch (InterruptedException exception) {
+            exception.printStackTrace();
+        }
+        chrono.stop();
 
-		
-	}
+
+    }
 
     public void actionPerformed(ActionEvent k){
-		for(int j = 0 ; j < 100 ; j++){
-        int i = 0;
-        Complexe lineEnd = new Complexe(0,0);
-        for(Complexe c : ligne){
-            lineEnd.sommeV1(c);
-            c.rotate(angleRot*i);
-            i++;
+        for(int j = 0 ; j < 100 ; j++){
+            int i = 0;
+            Complex lineEnd = new Complex(0,0);
+            for(Complex c : ligne){
+                lineEnd=lineEnd.plus(c);
+                c.rotate(angleRot*i);
+                i++;
+            }
+            dessin.add(lineEnd);
+            this.repaint();
         }
-        dessin.add(lineEnd);
-        this.repaint();
-		}
     }
 
     public void paint(Graphics h){
         h.setColor(Color.white);
         h.fillRect(0,0,getWidth(),getHeight());
         h.setColor(Color.black);
-        Complexe point1 = new Complexe(this.getWidth()/2, this.getHeight()/2);	//on place le premier point au milieu du panneau pour tracer le premier trait
-        for(Complexe c : ligne){
-            h.drawLine((int)(point1.getRe()), (int)(point1.getIm()), (int)(c.getRe() + point1.getRe()), (int)(c.getIm() + point1.getIm()));
-            point1 = new Complexe(c.getRe() + point1.getRe(), c.getIm() + point1.getIm());
+        Complex point1 = new Complex(this.getWidth()/2, this.getHeight()/2);	//on place le premier point au milieu du panneau pour tracer le premier trait
+        for(Complex c : ligne){
+            h.drawLine((int)(point1.re()), (int)(point1.im()), (int)(c.re() + point1.re()), (int)(c.im() + point1.im()));
+            point1 = new Complex(c.re() + point1.re(), c.im() + point1.im());
         }
-        point1 = new Complexe(this.getWidth()/2 + (int)(dessin.getFirst().getRe()), this.getHeight()/2 + (int)(dessin.getFirst().getIm()));
-        Complexe point0 = new Complexe(this.getWidth()/2 + (int)(dessin.getFirst().getRe()), this.getHeight()/2 + (int)(dessin.getFirst().getIm()));
-        for(Complexe c : dessin){
+        point1 = new Complex(this.getWidth()/2 + (int)(dessin.getFirst().re()), this.getHeight()/2 + (int)(dessin.getFirst().im()));
+        Complex point0 = new Complex(this.getWidth()/2 + (int)(dessin.getFirst().re()), this.getHeight()/2 + (int)(dessin.getFirst().im()));
+        for(Complex c : dessin){
             h.setColor(Color.red);
             if (point1.equals(point0)) {
-                h.setColor(Color.white); //de même, pour enlever le premier trait rouge qui connecte avec le milieu
+                h.setColor(Color.white); // pour enlever le premier trait rouge qui connecte avec le milieu
             }
-            h.drawLine((int)(point1.getRe()), (int)(point1.getIm()), (int)(c.getRe() + this.getWidth()/2), (int)(c.getIm()) + this.getHeight()/2);
-            point1 = new Complexe(c.getRe() + this.getWidth()/2, c.getIm() + this.getHeight()/2);
+            h.drawLine((int)(point1.re()), (int)(point1.im()), (int)(c.re() + this.getWidth()/2), (int)(c.im()) + this.getHeight()/2);
+            point1 = new Complex(c.re() + this.getWidth()/2, c.im() + this.getHeight()/2);
         }
-	}
-	
+    }
+
 
 
 
